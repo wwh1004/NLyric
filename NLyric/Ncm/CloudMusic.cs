87ea40e067600c83788100c319c725a192d19a2b
文煌 <wwh1004@hotmail.com>
2019-08-22 12:27:36 +0800

    更新API

--- NLyric/Ncm/CloudMusic.cs
+++ NLyric/Ncm/CloudMusic.cs
@@ -2,15 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NeteaseCloudMusicApi;
 using Newtonsoft.Json.Linq;
 using NLyric.Audio;
 using NLyric.Lyrics;
 
 namespace NLyric.Ncm {
 	public static class CloudMusic {
+		private static readonly CloudMusicApi _api = new CloudMusicApi();
+
 		public static async Task<NcmTrack[]> SearchTrackAsync(Track track, int limit, bool withArtists) {
 			List<string> keywords;
-			JToken json;
+			bool isOk;
+			JObject json;
 
 			keywords = new List<string>();
 			if (track.Name.Length != 0)
@@ -21,7 +25,16 @@ namespace NLyric.Ncm {
 				throw new ArgumentException("歌曲信息无效");
 			for (int i = 0; i < keywords.Count; i++)
 				keywords[i] = keywords[i].WholeWordReplace();
-			json = await NcmApi.SearchAsync(keywords, NcmApi.SearchType.Track, limit);
+			(isOk, json) = await _api.RequestAsync(CloudMusicApiProviders.Search, new Dictionary<string, string> {
+				{ "keywords", string.Join(" ", keywords) },
+				{ "type", "1" },
+				{ "limit", limit.ToString() }
+			});
+			if (!isOk)
+				throw new ApplicationException(nameof(CloudMusicApiProviders.Search) + " API错误");
+			json = (JObject)json["result"];
+			if (json is null)
+				throw new ApplicationException(nameof(CloudMusicApiProviders.Search) + " API错误");
 			if ((int)json["songCount"] == 0)
 				return Array.Empty<NcmTrack>();
 			return ((JArray)json["songs"]).Select(t => ParseTrack(t, false)).ToArray();
@@ -29,7 +42,8 @@ namespace NLyric.Ncm {
 
 		public static async Task<NcmAlbum[]> SearchAlbumAsync(Album album, int limit, bool withArtists) {
 			List<string> keywords;
-			JToken json;
+			bool isOk;
+			JObject json;
 
 			keywords = new List<string>();
 			if (album.Name.Length != 0)
@@ -40,27 +54,46 @@ namespace NLyric.Ncm {
 				throw new ArgumentException("专辑信息无效");
 			for (int i = 0; i < keywords.Count; i++)
 				keywords[i] = keywords[i].WholeWordReplace();
-			json = await NcmApi.SearchAsync(keywords, NcmApi.SearchType.Album, limit);
+			(isOk, json) = await _api.RequestAsync(CloudMusicApiProviders.Search, new Dictionary<string, string> {
+				{ "keywords", string.Join(" ", keywords) },
+				{ "type", "10" },
+				{ "limit", limit.ToString() }
+			});
+			if (!isOk)
+				throw new ApplicationException(nameof(CloudMusicApiProviders.Search) + " API错误");
+			json = (JObject)json["result"];
+			if (json is null)
+				throw new ApplicationException(nameof(CloudMusicApiProviders.Search) + " API错误");
 			if ((int)json["albumCount"] == 0)
 				return Array.Empty<NcmAlbum>();
 			return ((JArray)json["albums"]).Select(t => ParseAlbum(t)).ToArray();
 		}
 
 		public static async Task<NcmTrack[]> GetTracksAsync(int albumId) {
-			JToken json;
+			bool isOk;
+			JObject json;
 
-			json = await NcmApi.GetAlbumAsync(albumId);
+			(isOk, json) = await _api.RequestAsync(CloudMusicApiProviders.Album, new Dictionary<string, string> {
+				{ "id", albumId.ToString() }
+			});
+			if (!isOk)
+				throw new ApplicationException(nameof(CloudMusicApiProviders.Album) + " API错误");
 			return ((JArray)json["songs"]).Select(t => ParseTrack(t, true)).ToArray();
 		}
 
 		public static async Task<NcmLyric> GetLyricAsync(int trackId) {
-			JToken json;
+			bool isOk;
+			JObject json;
 			Lrc rawLrc;
 			int rawVersion;
 			Lrc translatedLrc;
 			int translatedVersion;
 
-			json = await NcmApi.GetLyricAsync(trackId);
+			(isOk, json) = await _api.RequestAsync(CloudMusicApiProviders.Lyric, new Dictionary<string, string> {
+				{ "id", trackId.ToString() }
+			});
+			if (!isOk)
+				throw new ApplicationException(nameof(CloudMusicApiProviders.Lyric) + " API错误");
 			if ((bool?)json["uncollected"] == true)
 				// 未收录
 				return new NcmLyric(trackId, false, false, null, 0, null, 0);
@@ -81,11 +114,11 @@ namespace NLyric.Ncm {
 			return ncmAlbum;
 		}
 
-		private static NcmTrack ParseTrack(JToken json, bool fromAlbumDetail) {
+		private static NcmTrack ParseTrack(JToken json, bool fromAlbum) {
 			Track track;
 			NcmTrack ncmTrack;
 
-			track = new Track((string)json["name"], ParseNames((JArray)json[fromAlbumDetail ? "ar" : "artists"]));
+			track = new Track((string)json["name"], ParseNames((JArray)json[fromAlbum ? "ar" : "artists"]));
 			ncmTrack = new NcmTrack(track, (int)json["id"]);
 			return ncmTrack;
 		}
